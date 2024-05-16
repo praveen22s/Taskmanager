8763a504b49e530477259514090af4a4468c9417
praveen22s <masspraveen2k4@gmail.com>
2024-05-16 20:21:02 +0530

    final

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import SideNavbar from "./components/SideNavbar";
+import User2 from "./components/User2";
+
+
+function App() {
+  return (
+    <div className="App">
+    <SideNavbar/>
+    <User2/>
+        </div>
+  );
+}
+
+export default App;
